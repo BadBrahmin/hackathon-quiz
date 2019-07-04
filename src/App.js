68f5929dc2455313ea48d0a8d3c0ba03861fb11a
souravbaranwal <sourav.baranwal@gmail.com>
2019-07-04 12:58:41 +0530

    boiler plate done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './App.scss';
-
+import React from "react";
+import "./App.scss";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 function App() {
   return (
-    <div className="App">
-      Hackthon Quiz app
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route render={() => <p>Not found</p>} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/test.js
@@ -0,0 +1,41 @@
+var questions = {
+    html : [{
+        title: 'Inside which HTML element do we put the JavaScript?',
+        options: [
+            '<scripting>',
+            '<script>',
+            '<js>',
+            'javascript'
+        ],
+        index: 0
+    },
+    {
+        title: 'Where is the correct place to insert a JavaScript?',
+        options: [
+            'The <body> section',
+            'The <head> section',
+            'Both are correct',
+        ],
+        index: 2
+    },
+    {
+        title: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        options: [
+            '<script href="xxx.js">',
+            '<script name="xxx.js">',
+            '<script src="xxx.js">',
+        ],
+        index: 2
+    },
+    {
+        title: 'The external JavaScript file must contain the <script> tag.',
+        options: [
+            'True',
+            'False',
+        ],
+        index: 1
+    },]
+}
+
+var data = 
+]
