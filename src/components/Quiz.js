61510c848d8f1d0fdeb986358e4fac72d569080e
prarora98 <prateekisthebest98@gmail.com>
2019-07-04 23:17:20 +0530

    css Reset

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,7 +3,12 @@ import React from "react";
 function Quiz() {
   return (
     <div className="wrapper">
-      <div className="quiz-sec">heelo</div>
+      <div className="quiz-sec">
+        <h2>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eos
+          voluptas modi labore?
+        </h2>
+      </div>
     </div>
   );
 }
