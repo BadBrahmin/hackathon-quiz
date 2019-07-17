d8f1e8dc8dc8a06f6e441adf5867b98f40533386
Gemahar Rafi <gemharedu@gmail.com>
2019-07-17 19:54:30 +0530

    Update variables.js

--- quiz-backend/config/variables.js
+++ quiz-backend/config/variables.js
@@ -1,18 +1,1 @@
 
-
-module.exports = {
-    githubClientId: 'Iv1.08047c88058ca0b1',
-    githubClientSecret: 'dd60ed40efd70c3f6ff2d558c26abe4ec441f0a1',
-    githubClientCallbackURL: 'http://localhost:4000/auth/github/callback',
-
-    googleClientId: '833290332163-5fj13tg1lfb0ekjkvqskm0889jnlc9sh.apps.googleusercontent.com',
-    googleClientSecret: 'hdGO1jSwhSFRdra_U4Yk2jgI',
-    googleClientCallbackURL: 'http://localhost:4000/auth/google/callback',
-
-    jwsSecret: "hjjd2357#@SWG"
-}
-
-
-// 
-
-// hdGO1jSwhSFRdra_U4Yk2jgI
